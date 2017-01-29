f30c4b61d00ade2e5643410ddbdf3b3449be1298
Steff McKee <steffmckee@outlook.com>
2017-01-29 13:53:19 -0700

    initial commit

---
+++ SimpleCSharpConsoleApp/SimpleCSharpConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up Console UI (CUI)
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*************************************");
+            Console.WriteLine("***** Welcome to My Rocking App *****");
+            Console.WriteLine("*************************************");
+
+            // Wait for Enter key to be pressed.
+            Console.ReadLine();
+        }
+    }
+}
